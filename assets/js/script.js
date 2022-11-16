758b14a6621beaf37bdcbe0988e4c68640545268
rubymaxioned <axioned0482022>
2022-11-16 13:31:57 +0530

    counter animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var btn = document.querySelector(".initial-content");
 var counterContent = document.querySelector(".counter-content");
 var counter = document.querySelector(".counter");
 
-console.log(counter);
+// console.log(counter);
 
 btn.addEventListener('click',myFunction);
 
@@ -16,17 +16,16 @@ function myFunction(){
 
     var interval = setInterval(function(){
         if(num < 0) {
+            console.log('abc');
             clearInterval(interval);
             btn.classList.remove('hide');
             counterContent.classList.remove('show');
         }
-        
+        else{
+            console.log('here');
+        counter.classList.add('active');
         counter.innerText = num;
         num--; 
+        }
     }, 1000)
-
-    // for(var i=0; i<3; i++){
-    //     var result = 3 - i;
-    //     console.log(result);
-    // }
 }
