9c45740660adabe09716760b1436e3bb8a8375ce
rubymaxioned <axioned0482022>
2022-11-16 14:50:23 +0530

    loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var btn = document.querySelector(".initial-content");
 var counterContent = document.querySelector(".counter-content");
-var counter = document.querySelector(".counter");
+var counter = document.querySelectorAll(".counter");
 
-// console.log(counter);
+counter[0].classList.add("active");
 
 btn.addEventListener('click',myFunction);
 
@@ -10,22 +10,40 @@ function myFunction(){
     btn.classList.add('hide');
     counterContent.classList.add('show');
 
-    let num = 2;
-
-    counter.innerText = 3;
-
-    var interval = setInterval(function(){
-        if(num < 0) {
-            console.log('abc');
+    var num = 1;
+    var interval = setInterval(function () {
+        if (num > 3) {
             clearInterval(interval);
             btn.classList.remove('hide');
             counterContent.classList.remove('show');
+        } else {
+            for(var i=0; i<counter.length; i++) {
+                counter[i].classList.remove("active");
+            }
+        
+            counter[num].classList.add("active");
         }
-        else{
-            console.log('here');
-        counter.classList.add('counter');
-        counter.innerText = num;
-        num--; 
-        }
-    }, 1000)
-}
+
+        num++;
+    }, 1000);
+};
+
+    
+//     let num = 2;
+
+//     counter.innerText = 3;
+
+//     var interval = setInterval(function(){
+//         if(num < 0) {
+//             console.log('abc');
+//             clearInterval(interval);
+//             btn.classList.remove('hide');
+//             counterContent.classList.remove('show');
+//         }
+//         else{
+//             console.log('here');
+//         counter.classList.add('counter');
+//         counter.innerText = num;
+//         num--; 
+//         }
+//     }, 1000)
